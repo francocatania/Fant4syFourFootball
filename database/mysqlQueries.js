50a9c6dc9e1835da12ea6ffa58d848916bdf905e
Franco Catania <catania.fco@gmail.com>
2018-01-11 11:19:18 -0800

    added playersInTeam query

--- database/mysqlQueries.js
+++ database/mysqlQueries.js
@@ -72,7 +72,19 @@ VALUES
 ?,
 ?)`;
 
-
+// NEED TO PASS username and week values as an argument in query.
+const playersInTeam = `SELECT players.id, players.name, players.position, playerStats.*, teams.id as teamId
+FROM playerStats
+INNER JOIN players
+ON playerStats.playerId = players.id
+INNER JOIN Teams_has_Players
+ON players.id = Teams_has_Players.Players_id
+INNER JOIN teams
+ON Teams_has_Players.Teams_id = teams.id
+INNER JOIN users
+ON teams.owner = users.id
+WHERE users.username = ?
+AND playerStats.week = ?`
 
 module.exports.savePlayerStats = savePlayerStats;
 module.exports.savePlayer = savePlayer;
