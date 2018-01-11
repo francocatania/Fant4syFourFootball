752ac3edd601ae7bd801c774c8b89752b83a66d8
David Harlow <davidaharlow@gmail.com>
2018-01-10 19:55:21 -0800

    (BUGFIX) Small patches

--- database/dummyData/hardCodedTeams.js
+++ database/dummyData/hardCodedTeams.js
@@ -1,14 +1,14 @@
 const teams = [
-  {id: 1, league: 'fantasyReactor', name: 'Team_1', owner: null, players: [], matches: []},
-  {id: 2, league: 'fantasyReactor', name: 'Team_2', owner: null, players: [], matches: []},
-  {id: 3, league: 'fantasyReactor', name: 'Team_3', owner: null, players: [], matches: []},
-  {id: 4, league: 'fantasyReactor', name: 'Team_4', owner: null, players: [], matches: []},
-  {id: 5, league: 'fantasyReactor', name: 'Team_5', owner: null, players: [], matches: []},
-  {id: 6, league: 'fantasyReactor', name: 'Team_6', owner: null, players: [], matches: []},
-  {id: 7, league: 'fantasyReactor', name: 'Team_7', owner: null, players: [], matches: []},
-  {id: 8, league: 'fantasyReactor', name: 'Team_8', owner: null, players: [], matches: []},
-  {id: 9, league: 'fantasyReactor', name: 'Team_9', owner: null, players: [], matches: []},
-  {id: 10, league: 'fantasyReactor', name: 'Team_10', owner: null, players: [], matches: []},
-  {id: 11, league: 'fantasyReactor', name: 'Team_11', owner: null, players: [], matches: []},
-  {id: 12, league: 'fantasyReactor', name: 'Team_12', owner: null, players: [], matches: []}
+  {id: 1, league: 'fantasyReactor', name: 'Hide & Zeke', owner: null, players: []},
+  {id: 2, league: 'fantasyReactor', name: 'GurleyThings', owner: null, players: []},
+  {id: 3, league: 'fantasyReactor', name: 'The Walking Dez', owner: null, players: []},
+  {id: 4, league: 'fantasyReactor', name: 'Inglourious Bradfords', owner: null, players: []},
+  {id: 5, league: 'fantasyReactor', name: 'Mariota Had a Little Lamb', owner: null, players: []},
+  {id: 6, league: 'fantasyReactor', name: 'InstaJimmyGraham', owner: null, players: []},
+  {id: 7, league: 'fantasyReactor', name: 'Ladies and Edelman', owner: null, players: []},
+  {id: 8, league: 'fantasyReactor', name: 'Cry Me a Rivers', owner: null, players: []},
+  {id: 9, league: 'fantasyReactor', name: 'Brady Gaga', owner: null, players: []},
+  {id: 10, league: 'fantasyReactor', name: 'Stairway to Evans', owner: null, players: []},
+  {id: 11, league: 'fantasyReactor', name: 'Turn Down for Watt', owner: null, players: []},
+  {id: 12, league: 'fantasyReactor', name: 'Kung Suh Panda', owner: null, players: []}
 ];
