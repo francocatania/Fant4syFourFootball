ada058d6b827f0d92221568dc459c0999c007aac
Franco Catania <catania.fco@gmail.com>
2018-01-10 19:28:03 -0800

    (refactor) savePlayer & savePlayerStats queries

--- database/mysqlQueries.js
+++ database/mysqlQueries.js
@@ -1,11 +1,79 @@
-const savePlayerStats = ``;
-
 const savePlayer = `INSERT INTO players
 (id, name, position)
 VALUES
-(${player.id}, ${player.name}, ${player.position})`;
+(?, ?, ?)`;
+
+const allStats = ['week',
+    'playerID',
+    'passingYards',
+    'passingTouchdowns',
+    'passingInterceptions',
+    'rushingYards',
+    'rushingTouchdowns',
+    'receptions',
+    'receivingYards',
+    'receivingTouchdowns',
+    'fumbles',
+    'fieldGoalsMade0to19',
+    'fieldGoalsMade20to29',
+    'fieldGoalsMade30to39',
+    'fieldGoalsMade40to49',
+    'fieldGoalsMade50Plus',
+    'extraPointsMade',
+    'twoPointConversionPasses',
+    'twoPointConversionRuns',
+    'twoPointConversionReceptions',
+    'isGameOver'];
+    
+const savePlayerStats = `INSERT INTO playerStats
+(week,
+playerID,
+passingYards,
+passingTouchdowns,
+passingInterceptions,
+rushingYards,
+rushingTouchdowns,
+receptions,
+receivingYards,
+receivingTouchdowns,
+fumbles,
+fieldGoalsMade0to19,
+fieldGoalsMade20to29,
+fieldGoalsMade30to39,
+fieldGoalsMade40to49,
+fieldGoalsMade50Plus,
+extraPointsMade,
+twoPointConversionPasses,
+twoPointConversionRuns,
+twoPointConversionReceptions,
+isGameOver)
+
+VALUES
+
+?,
+?,
+?,
+?,
+?,
+?,
+?,
+?,
+?,
+?,
+?,
+?,
+?,
+?,
+?,
+?,
+?,
+?,
+?,
+?,
+?)`;
 
 
 
 module.exports.savePlayerStats = savePlayerStats;
 module.exports.savePlayer = savePlayer;
+module.exports.allStats = allStats;
