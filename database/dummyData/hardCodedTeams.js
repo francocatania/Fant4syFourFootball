26868e6de6311a456225e48843125270e2354e67
David Harlow <davidaharlow@gmail.com>
2018-01-10 20:03:44 -0800

    updated teams data

--- database/dummyData/hardCodedTeams.js
+++ database/dummyData/hardCodedTeams.js
@@ -1,14 +1,14 @@
 const teams = [
-  {id: 1, league: 'fantasyReactor', name: 'Hide & Zeke', owner: null, players: []},
-  {id: 2, league: 'fantasyReactor', name: 'GurleyThings', owner: null, players: []},
-  {id: 3, league: 'fantasyReactor', name: 'The Walking Dez', owner: null, players: []},
-  {id: 4, league: 'fantasyReactor', name: 'Inglourious Bradfords', owner: null, players: []},
-  {id: 5, league: 'fantasyReactor', name: 'Mariota Had a Little Lamb', owner: null, players: []},
-  {id: 6, league: 'fantasyReactor', name: 'InstaJimmyGraham', owner: null, players: []},
-  {id: 7, league: 'fantasyReactor', name: 'Ladies and Edelman', owner: null, players: []},
-  {id: 8, league: 'fantasyReactor', name: 'Cry Me a Rivers', owner: null, players: []},
-  {id: 9, league: 'fantasyReactor', name: 'Brady Gaga', owner: null, players: []},
-  {id: 10, league: 'fantasyReactor', name: 'Stairway to Evans', owner: null, players: []},
-  {id: 11, league: 'fantasyReactor', name: 'Turn Down for Watt', owner: null, players: []},
-  {id: 12, league: 'fantasyReactor', name: 'Kung Suh Panda', owner: null, players: []}
+  {id: 1, league: 1, name: 'Hide & Zeke', owner: null},
+  {id: 2, league: 1, name: 'GurleyThings', owner: null},
+  {id: 3, league: 1, name: 'The Walking Dez', owner: null},
+  {id: 4, league: 1, name: 'Inglourious Bradfords', owner: null},
+  {id: 5, league: 1, name: 'Mariota Had a Little Lamb', owner: null},
+  {id: 6, league: 1, name: 'InstaJimmyGraham', owner: null},
+  {id: 7, league: 1, name: 'Ladies and Edelman', owner: null},
+  {id: 8, league: 1, name: 'Cry Me a Rivers', owner: null},
+  {id: 9, league: 1, name: 'Brady Gaga', owner: null},
+  {id: 10, league: 1, name: 'Stairway to Evans', owner: null},
+  {id: 11, league: 1, name: 'Turn Down for Watt', owner: null},
+  {id: 12, league: 1, name: 'Kung Suh Panda', owner: null}
 ];
