4ebdaf2b431eb168d7d8b3b4d6356e83469ac373
Reenuka Sarika Rāmphal <reenuka@Reenukas-MacBook-Pro.local>
2018-01-12 14:43:58 -0800

    add login/sigup server helpers

--- server/serverHelpers.js
+++ server/serverHelpers.js
@@ -35,6 +35,12 @@ const addUser = (username, password, callback) => {
   const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   let userId = ids.pop();
   db.saveUser(username, password, userId);
-  db.assignTeam(userId);
+  // db.assignTeam(userId);
   getUserInfo(userId);
 };
+
+module.exports= {
+  authenticate,
+  getUserInfo,
+  addUser
+}
