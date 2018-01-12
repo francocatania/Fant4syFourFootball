4c1d0af98f3486dbedb8a757f80a69fce41ad491
Michael Cooper <mcooper92@gmail.com>
2018-01-12 11:49:25 -0800

    Add profileCard component and teamTable components, which then render through myTeam. Hardcoded is currently rendered, dynamic data commented out at the bottom

--- client/src/components/teamTable.jsx
+++ client/src/components/teamTable.jsx
@@ -1,10 +1,4 @@
 import React from 'react';
-import Avatar from 'material-ui/Avatar';
-import FileFolder from 'material-ui/svg-icons/file/folder';
-import FontIcon from 'material-ui/FontIcon';
-import List from 'material-ui/List/List';
-import ListItem from 'material-ui/List/ListItem';
-
 import {
   Table,
   TableBody,
@@ -13,8 +7,6 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
-
-
 
 
 const TeamTable = (props) => {
@@ -30,20 +22,96 @@ const TeamTable = (props) => {
       </TableRow>
     </TableHeader>
     <TableBody>
-      {props.players.map((player, index) =>{
-          return <TableRow>
-                 <TableRowColumn>{player.position}</TableRowColumn>
-                 <TableRowColumn>{player.name}</TableRowColumn>
-                 <TableRowColumn>5</TableRowColumn>
-                 <TableRowColumn>12.16</TableRowColumn>
-                 </TableRow>
-      })}
-      
+      <TableRow>
+        <TableRowColumn>QB</TableRowColumn>
+        <TableRowColumn>Drew Brees</TableRowColumn>
+        <TableRowColumn>5</TableRowColumn>
+        <TableRowColumn>12.16</TableRowColumn>
+      </TableRow>
+      <TableRow>
+        <TableRowColumn>WR</TableRowColumn>
+        <TableRowColumn>Demaryius Thomas</TableRowColumn>
+        <TableRowColumn>5</TableRowColumn>
+        <TableRowColumn>9.20</TableRowColumn>
+      </TableRow>
+      <TableRow>
+        <TableRowColumn>WR</TableRowColumn>
+        <TableRowColumn>Robby Anderson</TableRowColumn>
+        <TableRowColumn>11</TableRowColumn>
+        <TableRowColumn>10.10</TableRowColumn>
+      </TableRow>
+      <TableRow>
+      <TableRowColumn>RB</TableRowColumn>
+        <TableRowColumn>LeSean McCoy</TableRowColumn>
+        <TableRowColumn>6</TableRowColumn>
+        <TableRowColumn>19.70</TableRowColumn>
+      </TableRow>
+      <TableRow>
+        <TableRowColumn>RB</TableRowColumn>
+        <TableRowColumn>Rex Burkhead</TableRowColumn>
+        <TableRowColumn>9</TableRowColumn>
+        <TableRowColumn>17.89</TableRowColumn>
+      </TableRow>
+      <TableRow>
+        <TableRowColumn>TE</TableRowColumn>
+        <TableRowColumn>Delanie Walker</TableRowColumn>
+        <TableRowColumn>8</TableRowColumn>
+        <TableRowColumn>6.30</TableRowColumn>
+      </TableRow>
+      <TableRow>
+        <TableRowColumn>FLEX</TableRowColumn>
+        <TableRowColumn>Devin Funchess</TableRowColumn>
+        <TableRowColumn>11</TableRowColumn>
+        <TableRowColumn>4.10</TableRowColumn>
+      </TableRow>
+      <TableRow>
+        <TableRowColumn>K</TableRowColumn>
+        <TableRowColumn>Blair Walsh</TableRowColumn>
+        <TableRowColumn>6</TableRowColumn>
+        <TableRowColumn>6</TableRowColumn>
+      </TableRow>
+      <TableRow>
+        <TableRowColumn>D/ST</TableRowColumn>
+        <TableRowColumn>Minnesota D/ST</TableRowColumn>
+        <TableRowColumn>7</TableRowColumn>
+        <TableRowColumn>7</TableRowColumn>
+      </TableRow>
     </TableBody>
-  </Table> 
+  </Table>
   
 
     );
   }
 
 export default TeamTable
+
+
+
+// const TeamTable = (props) => {
+//   return (
+
+//   <Table>
+//     <TableHeader>
+//       <TableRow>
+//         <TableHeaderColumn>QB</TableHeaderColumn>
+//         <TableHeaderColumn>Offense</TableHeaderColumn>
+//         <TableHeaderColumn>Bye</TableHeaderColumn>
+//         <TableHeaderColumn>Points</TableHeaderColumn>
+//       </TableRow>
+//     </TableHeader>
+//     <TableBody>
+//       {props.players.map((player, index) =>{
+//           return <TableRow>
+//                  <TableRowColumn>{player.position}</TableRowColumn>
+//                  <TableRowColumn>{player.name}</TableRowColumn>
+//                  <TableRowColumn>5</TableRowColumn>
+//                  <TableRowColumn>12.16</TableRowColumn>
+//                  </TableRow>
+//       })}
+      
+//     </TableBody>
+//   </Table> 
+  
+
+//     );
+//   }
