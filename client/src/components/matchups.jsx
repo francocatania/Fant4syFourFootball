0594886bb6d4658bfb1059c2f4b2e41c5a43008a
Franco Catania <catania.fco@gmail.com>
2018-01-12 19:35:08 -0800

    getTeamScore

--- client/src/components/matchups.jsx
+++ client/src/components/matchups.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import {getPlayerScore, fakePlayer} from '../helpers.js'
 
-const Matchups = () => {
-  return (
-    <p>Matchups</p>
-  );
+class Matchups extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userTeam: [fakePlayer], // array of objects containing both player info and stats
+      userTeamScore: 0,
+      rivalTeam: [],
+      rivalTeamScore: 0
+    }
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  getTeamScore(team) {
+    let totalPoints = 0;
+    team.forEach( player => {
+      totalPoints += getPlayerScore(player);
+    })
+    return totalPoints;
+  }
+
+  componentDidMount() {
+    let userTeamScore = this.getTeamScore(this.state.userTeam);
+    let rivalTeamScore = this.getTeamScore(this.state.rivalTeam);
+
+    this.setState({userTeamScore: userTeamScore});
+    this.setState({rivalTeamScore: rivalTeamScore});
+  }
+
+  render() {
+    return (
+      <div>
+        <p> Maaachop </p>
+      </div>
+    );
+  }
+
 }
 
-export default Matchups
+export default Matchups;
---
+++ client/src/helpers.js
@@ -0,0 +1,45 @@
+const getPlayerScore = player => {
+    let total = 0;
+    total += player.passingYards * 0.025;
+    total += player.PassingTouchdowns * 4;
+    total += player.passingInterceptions * -1;
+    total += player.rushingYards * 0.1;
+    total += player.rushingTouchdowns * 6;
+    total += player.receptions * 0.5;
+    total += player.receivingYards * 0.1;
+    total += player.receivingTouchdowns * 6;
+    total += player.fumbles * -1;
+    total += player.fieldGoalsMade0to19 * 3;
+    total += player.fieldGoalsMade20to29 * 3
+    total += player.fieldGoalsMade30to39 * 3
+    total += player.fieldGoalsMade40to49 * 4;
+    total += player.fieldGoalsMade50Plus * 5;
+    total += player.extraPointsMade;
+
+    return total;
+}
+
+const fakePlayer = {
+    id: 1234,
+    name: 'Frank The Tank',
+    position: 'QB',
+    passingYards: 146,
+    PassingTouchdowns: 3,
+    passingInterceptions: 0,
+    rushingYards: 57,
+    rushingTouchdowns: 1,
+    receptions: 0,
+    receivingYards: 0,
+    receivingTouchdowns: 0,
+    fumbles: 0,
+    fieldGoalsMade0to19: 0,
+    fieldGoalsMade20to29: 0,
+    fieldGoalsMade30to39: 0,
+    fieldGoalsMade40to49: 0,
+    fieldGoalsMade50Plus: 0,
+    extraPointsMade: 2,
+
+}
+
+module.exports.getPlayerScore = getPlayerScore;
+module.exports.fakePlayer = fakePlayer;
