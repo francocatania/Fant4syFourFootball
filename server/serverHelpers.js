ee3c95bcb7a9edea4d17aa6dd1d657194319b605
Michael Cooper <mcooper92@gmail.com>
2018-01-13 23:24:20 -0800

    added updateWinLosses to serverHelpers

--- server/serverHelpers.js
+++ server/serverHelpers.js
@@ -1,1 +1,11 @@
+const db = require('../database/index.js');
 
+const updateWinsLosses = (id, result, res) => {
+    if (result === 'win') {
+        db.updateWins(id, res);
+    } else if (result === 'loss') {
+        db.updateLosses(id, res);
+    }
+};
+
+module.exports.updateWinsLosses = updateWinsLosses;
