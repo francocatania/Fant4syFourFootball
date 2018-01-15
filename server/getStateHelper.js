58ed38d6e96a10bffb834573b8e18cfeb0cc01bc
Franco Catania <catania.fco@gmail.com>
2018-01-15 14:34:47 -0800

    changed domain staging path

--- server/getStateHelper.js
+++ server/getStateHelper.js
@@ -8,7 +8,7 @@ if (process.env.PORT) {
   domain = 'https://fant4syfootball.herokuapp.com'
 }
 if (process.env.inStaging === 'true') {
-  domain = 'https://fant4syfootball-staging.herokuapp.com'
+  domain = 'fant4syfootball-staging.herokuapp.com'
 }
 
 console.log('domain = ', domain);
