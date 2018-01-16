6ef48b889e8512023641d746045b9ecb173f3fe2
Reenuka Sarika Rāmphal <reenuka@Reenukas-MacBook-Pro.local>
2018-01-15 16:17:09 -0800

    minor styling to home page

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -35,7 +35,7 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <h1>Welcome {this.props.user}!</h1>
+        <h1 id="welcome">Welcome {this.props.user}!</h1>
         <div>
           {articles.docs.map((article) =>
              <Article
